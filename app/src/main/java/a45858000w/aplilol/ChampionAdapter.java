6a256fc1ca2e0a3d247ba54417a686c7568e4f21
CristianJRamirez <45858000w@iespoblenou.org>
2016-12-13 17:08:20 +0100

    Sustituimos las llamadas al findViewById por el objeto Binding en el adapterChampion, y actualizamos la vista de la lista de campeons

--- app/src/main/java/a45858000w/aplilol/ChampionAdapter.java
+++ app/src/main/java/a45858000w/aplilol/ChampionAdapter.java
@@ -1,6 +1,7 @@
 package a45858000w.aplilol;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
+
+import a45858000w.aplilol.databinding.ChampionLayoutBinding;
 
 /**
  * Created by 45858000w on 09/12/16.
@@ -29,25 +32,31 @@ public class ChampionAdapter extends ArrayAdapter<Champion> {
         // Obtenim l'objecte en la possició corresponent
         Champion c  = getItem(position);
         Log.w("XXXX", c.toString());
+
+        ChampionLayoutBinding binding = null;
+
         // Mirem a veure si la View s'està reusant, si no es així "inflem" la View
         // https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView#row-view-recycling
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.champion_layout, parent, false);
+            //convertView = inflater.inflate(R.layout.champion_layout, parent, false);
+            binding = DataBindingUtil.inflate(inflater, R.layout.champion_layout, parent, false);
+                }
+        else {
+                    binding = DataBindingUtil.getBinding(convertView);
         }
 
-        // Unim el codi en les Views del Layout
-        TextView Champion = (TextView) convertView.findViewById(R.id.txtChampion);
-        TextView title = (TextView) convertView.findViewById(R.id.titulo);
-        ImageView Image = (ImageView) convertView.findViewById(R.id.ImageChamp);
+
+
+
 
         // Fiquem les dades dels objectes (provinents del JSON) en el layout
-        Champion.setText(c.getName());
-        title.setText(c.getTitle());
-        Glide.with(getContext()).load(c.getImageSquareFull()).into(Image);
+        binding.txtChampion2.setText(c.getName());
+        binding.txtChampion.setText(c.getTitle());
+        Glide.with(getContext()).load(c.getImageSquareFull()).into(binding.ImageChamp);
 
 
         // Retornem la View replena per a mostrarla
-        return convertView;
+        return binding.getRoot();
     }
 }
