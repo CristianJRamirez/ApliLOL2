200615267d8196735c0efe9888d4620d658fb116
CristianJRamirez <45858000w@iespoblenou.org>
2017-01-09 16:44:21 +0100

    Hacemos el cliente de l'api estatico por estalviar inicialitzaciones innecesaries

--- app/src/main/java/a45858000w/aplilol/Api.java
+++ app/src/main/java/a45858000w/aplilol/Api.java
@@ -20,28 +20,28 @@ import a45858000w.aplilol.DatosSecundarios.InfoEstadisticas;
  */
 
 public class Api {
-    private String InfoApi="https://developer.riotgames.com/api/methods#!/1206/4678";
-    private String InfoData="https://developer.riotgames.com/docs/static-data";
+    private static String InfoApi="https://developer.riotgames.com/api/methods#!/1206/4678";
+    private static String InfoData="https://developer.riotgames.com/docs/static-data";
 
-    private String Api_KEY="RGAPI-f53a2e9d-2955-4e4e-9582-6725ed511ba3";
-    private String AllChampion="https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key="+Api_KEY;
+    private static String Api_KEY="RGAPI-f53a2e9d-2955-4e4e-9582-6725ed511ba3";
+    private static String AllChampion="https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key="+Api_KEY;
 
-    private String IdChampion;
+    private static String IdChampion;
 
-    private String ChampionID ="https://euw.api.pvp.net/api/lol/euw/v1.2/champion/"+IdChampion+"?api_key="+Api_KEY;
+    private static String ChampionID ="https://euw.api.pvp.net/api/lol/euw/v1.2/champion/"+IdChampion+"?api_key="+Api_KEY;
 
 
-    private String ChampionLista="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key="+Api_KEY;//con nombres, ID, titulo,key
-    private String ChampionListaImagenes="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image&api_key="+Api_KEY;//con nombres, ID, titulo,key
-    private  String ChampionIDDetalle="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+IdChampion+"?api_key="+Api_KEY;//detelles del champion
-    private  String ChampionIDDetalleImagen="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+IdChampion+"?champData=image&api_key="+Api_KEY;//detelles del champion
+    private static String ChampionLista="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key="+Api_KEY;//con nombres, ID, titulo,key
+    private static String ChampionListaImagenes="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image&api_key="+Api_KEY;//con nombres, ID, titulo,key
+    private static String ChampionIDDetalle="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+IdChampion+"?api_key="+Api_KEY;//detelles del champion
+    private static String ChampionIDDetalleImagen="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+IdChampion+"?champData=image&api_key="+Api_KEY;//detelles del champion
     //private String ChampionAllDetail ="https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+IdChampion+"?champData=all&api_key="+Api_KEY;
 
 
 
     //https://api.magicthegathering.io/v1/cards
     //  String getCartas(String pais) { //para utilizar el parametro
-   public String getChampions(){
+   public static String getChampions(){
         Uri builtUri = Uri.parse(ChampionListaImagenes)
                 .buildUpon()
                 //.appendPath("box_office.json")
@@ -58,7 +58,7 @@ public class Api {
         return null;
     }
 
-    public ArrayList<Champion> getAllChampions(){
+    public static ArrayList<Champion> getAllChampions(){
         Uri builtUri = Uri.parse("https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=RGAPI-f53a2e9d-2955-4e4e-9582-6725ed511ba3")
                 .buildUpon()
                 //.appendPath("box_office.json")
@@ -100,7 +100,7 @@ public class Api {
         return null;
     }
 
-    private Champion getDetallesId(String id) throws JSONException {
+    private static Champion getDetallesId(String id) throws JSONException {
 
         Uri builtUri = Uri.parse("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+id+"?champData=all&api_key="+Api_KEY)
                 .buildUpon()
@@ -162,7 +162,7 @@ public class Api {
 
     }
 
-    public ArrayList<Champion> getAllChampions(String nombre) {
+    public static ArrayList<Champion> getAllChampions(String nombre) {
         ArrayList<Champion> champsFiltro =new ArrayList<>();
         ArrayList<Champion> champs =getAllChampions();
         for (Champion c: champs ) {
