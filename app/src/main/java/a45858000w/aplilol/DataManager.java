9724d924562ac7dff2e17a139517309ae1f3e3d3
CristianJRamirez <45858000w@iespoblenou.org>
2017-01-10 17:33:16 +0100

    añadido el metodo que te devuelve el filtro desde la bbdd

--- app/src/main/java/a45858000w/aplilol/DataManager.java
+++ app/src/main/java/a45858000w/aplilol/DataManager.java
@@ -35,4 +35,20 @@ public class DataManager {
         return new CursorLoader(context, CHAMPION_URI, null, null, null, null);
     }//TODO: buscar los datos que van en estos nulos, que ahi van los select y los where de la bbdd y desde ahi podemos recoger los datos
 
+
+    static CursorLoader getCursorLoaderPrueba(Context context,String[] projection,String where,String[] whereargs,String order){
+        return new CursorLoader(context,CHAMPION_URI,projection,where,whereargs,order);
+    }
+
+
+
+    /*
+    CursorLoader(Context context, Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder)
+    El significado de los parámetros son los siguientes:
+        Context: Contexto del entorno de aplicación.
+        Uri: URI con contenido: // patrón que recupera el contenido.
+        Projection: Lista de columnas a devolver y si null devolverá todas las columnas.
+        Selección: Filtro para devolver filas. Formato es como consulta SQL.
+        SortOrder: Tipo de orden como SQL ORDER BY y si nulo, la salida será desordenada.
+     */
 }
